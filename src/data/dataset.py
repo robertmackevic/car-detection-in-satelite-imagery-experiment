Updated model; added training data transformation, removed class computation, changed loss function, fixed metrics calculations.

src/data/dataset.py
@@ -6,7 +6,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 
 from src.data.entry import ImageEntry
-from src.data.transforms import get_inference_transform
+from src.data.transforms import get_training_transform
 
 
 class ObjectDetectionDataset(Dataset):
@@ -17,9 +17,8 @@ class ObjectDetectionDataset(Dataset):
 
         self.num_x_cells, self.num_y_cells = config["grid"]
         self.boxes_per_cell = config["boxes_per_cell"]
-        self.num_classes = config["classes"]
 
-        self.transform = get_inference_transform(
+        self.transform = get_training_transform(
             image_size=config["image_size"],
             in_channels=config["in_channels"],
         )
@@ -31,7 +30,7 @@ class ObjectDetectionDataset(Dataset):
         image = Image.fromarray(self.entries[index].image)
         image = self.transform(image)
 
-        grid = torch.zeros((self.num_y_cells, self.num_x_cells, self.num_classes + 5 * self.boxes_per_cell))
+        grid = torch.zeros((self.num_y_cells, self.num_x_cells, 5 * self.boxes_per_cell))
 
         for annotation in self.entries[index].annotations:
             x = self.num_x_cells * annotation.x
@@ -47,15 +46,11 @@ class ObjectDetectionDataset(Dataset):
             cell_h = annotation.height * self.num_y_cells
 
             # One object per cell
-            if grid[cell_row, cell_column, self.num_classes] == 0:
-                grid[cell_row, cell_column, self.num_classes] = 1
+            if grid[cell_row, cell_column, 0] == 0:
+                grid[cell_row, cell_column, 0] = 1
 
                 bbox = Tensor([cell_x, cell_y, cell_w, cell_h])
-                grid[cell_row, cell_column, self.num_classes + 1: self.num_classes + 5] = bbox
-
-                class_id = 0 if self.num_classes == 1 else annotation.class_id
-                # One-hot encoding class_id
-                grid[cell_row, cell_column, class_id] = 1
+                grid[cell_row, cell_column, 1: 5] = bbox
 
         return image, grid
 

src/model/detector.py
@@ -1,5 +1,5 @@
 from dataclasses import replace
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 import torch
 from PIL import Image
@@ -16,8 +16,8 @@ class Detector:
         self.model = model
         self.num_x_cells, self.num_y_cells = config["grid"]
         self.num_cells = self.num_x_cells * self.num_y_cells
+
         self.boxes_per_cell = config["boxes_per_cell"]
-        self.num_classes = config["classes"]
         self.image_size = config["image_size"]
         self.iou_threshold = config["iou_threshold"]
 
@@ -26,7 +26,7 @@ class Detector:
             in_channels=config["in_channels"],
         )
 
-    def detect(self, entry: ImageEntry, threshold: float = .5) -> ImageEntry:
+    def detect(self, entry: ImageEntry, threshold: float = 0.5) -> ImageEntry:
         patches = entry_to_patches(entry, patch_size=self.image_size)
         source = torch.cat([self.transform(Image.fromarray(patch.image)).unsqueeze(0) for patch in patches], dim=0)
         batch_size = source.shape[0]
@@ -45,11 +45,10 @@ class Detector:
             annotations = []
             for bbox in nms_boxes:
                 annotations.append(Annotation(
-                    class_id=round(bbox[0]),
-                    x=bbox[2],
-                    y=bbox[3],
-                    width=bbox[4],
-                    height=bbox[5],
+                    x=bbox[1],
+                    y=bbox[2],
+                    width=bbox[3],
+                    height=bbox[4],
                 ))
 
             patches[idx] = replace(patches[idx], annotations=annotations)
@@ -57,39 +56,40 @@ class Detector:
         result = patches_to_entry(patches)
         return result
 
-    def cellboxes_to_boxes(self, cells: Tensor):
-        converted_pred = self.convert_cellboxes(cells).reshape(cells.shape[0], self.num_cells, -1)
+    def cellboxes_to_boxes(self, grid: Tensor) -> List:
+        converted_pred = self.convert_cellboxes(grid).reshape(grid.shape[0], self.num_cells, -1)
         converted_pred[..., 0] = converted_pred[..., 0].long()
         all_bboxes = []
 
-        for ex_idx in range(cells.shape[0]):
+        for score_idx in range(grid.shape[0]):
             bboxes = []
 
             for bbox_idx in range(self.num_cells):
-                bboxes.append([x.item() for x in converted_pred[ex_idx, bbox_idx, :]])
+                bboxes.append([x.item() for x in converted_pred[score_idx, bbox_idx, :]])
             all_bboxes.append(bboxes)
 
         return all_bboxes
 
-    def convert_cellboxes(self, cells: Tensor) -> Tensor:
-        cells = cells.to("cpu")
-        batch_size = cells.shape[0]
+    def convert_cellboxes(self, grid: Tensor) -> Tensor:
+        grid = grid.to("cpu")
+        batch_size = grid.shape[0]
 
-        cells = cells.reshape(
-            batch_size, self.num_y_cells, self.num_x_cells, self.num_classes + self.boxes_per_cell * 5)
+        grid = grid.reshape(
+            batch_size, self.num_y_cells, self.num_x_cells, self.boxes_per_cell * 5)
 
         bboxes = [
-            cells[..., self.num_classes + 1 + (5 * i): self.num_classes + 5 + (5 * i)]
+            grid[..., 1 + (5 * i): 5 + (5 * i)]
             for i in range(self.boxes_per_cell)
         ]
 
         scores = torch.cat([
-            cells[..., self.num_classes + (5 * i)].unsqueeze(0)
+            grid[..., 5 * i].unsqueeze(0)
             for i in range(self.boxes_per_cell)
         ], dim=0)
 
         best_box = scores.argmax(0).unsqueeze(-1)
         best_boxes = torch.zeros_like(bboxes[0])
+
         for i in range(self.boxes_per_cell):
             best_boxes += best_box.eq(i).float() * bboxes[i]
 
@@ -101,12 +101,9 @@ class Detector:
 
         converted_bboxes = torch.cat((x, y, w_y), dim=-1)
 
-        predicted_class = cells[..., :self.num_classes].argmax(-1).unsqueeze(-1)
-
         best_confidence = torch.max(
             torch.stack([
-                cells[..., self.num_classes + (5 * i)] for i in range(self.boxes_per_cell)
+                grid[..., 5 * i] for i in range(self.boxes_per_cell)
             ], dim=0), dim=0).values.unsqueeze(-1)
 
-        converted_preds = torch.cat((predicted_class, best_confidence, converted_bboxes), dim=-1)
-        return converted_preds
+        return torch.cat((best_confidence, converted_bboxes), dim=-1)

src/model/loss.py
@@ -15,40 +15,37 @@ class YoloLoss(Module):
         self.num_x_cells, self.num_y_cells = config["grid"]
         self.num_cells = self.num_x_cells * self.num_y_cells
         self.boxes_per_cell = config["boxes_per_cell"]
-        self.num_classes = config["classes"]
+
         self.lambda_noobj = config["lambda_noobj"]
         self.lambda_coord = config["lambda_coord"]
+        self.lambda_obj = config["lambda_obj"]
 
     def forward(self, predictions: Tensor, target: Tensor) -> Tuple:
-        predictions = predictions.reshape(
-            -1, self.num_y_cells, self.num_x_cells, self.num_classes + self.boxes_per_cell * 5)
-        bbox_idx_start = self.num_classes + 1
-        bbox_idx_end = self.num_classes + 5
-        score_idx_start = self.num_classes
-        score_idx_end = bbox_idx_start
+        predictions = predictions.reshape(-1, self.num_y_cells, self.num_x_cells, self.boxes_per_cell * 5)
 
         ious = torch.cat([
             intersection_over_union(
-                prediction=predictions[..., bbox_idx_start + (5 * i):bbox_idx_end + (5 * i)],
-                target=target[..., bbox_idx_start:bbox_idx_end]
+                prediction=predictions[..., 1 + (5 * i):5 + (5 * i)],
+                target=target[..., 1:5]
             ).unsqueeze(0)
             for i in range(self.boxes_per_cell)
         ], dim=0)
 
         best_box = torch.argmax(ious, dim=0)
-        object_exists = target[..., self.num_classes].unsqueeze(3)
+        object_exists = target[..., 0].unsqueeze(3)
 
         # BOX LOSS
-        box_predictions = self._collect_predictions(predictions, best_box, bbox_idx_start, bbox_idx_end)
+        box_predictions = self._collect_predictions(predictions, best_box, start_idx=1, end_idx=5)
 
         # For boxes with no object in them, set box_predictions to 0
         box_predictions = object_exists * box_predictions
 
-        box_targets = object_exists * target[..., bbox_idx_start:bbox_idx_end]
-
-        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
-            torch.abs(box_predictions[..., 2:4] + 1e-6))
+        box_targets = object_exists * target[..., 1:5]
         box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
+        box_predictions[..., 2:4] = (
+                torch.sign(box_predictions[..., 2:4]) *
+                torch.sqrt(torch.abs(box_predictions[..., 2:4] + 1e-6))
+        )
 
         box_loss = self.mse(
             torch.flatten(box_predictions, end_dim=-2),
@@ -56,11 +53,11 @@ class YoloLoss(Module):
         )
 
         # OBJECT LOSS
-        pred_obj_confidence = self._collect_predictions(predictions, best_box, score_idx_start, score_idx_end)
+        pred_obj_confidence = self._collect_predictions(predictions, best_box, start_idx=0, end_idx=1)
 
         object_loss = self.mse(
             torch.flatten(object_exists * pred_obj_confidence),
-            torch.flatten(object_exists * target[..., score_idx_start:score_idx_end]),
+            torch.flatten(object_exists * target[..., 0:1]),
         )
 
         # NO OBJECT LOSS
@@ -71,29 +68,22 @@ class YoloLoss(Module):
 
             no_object_loss += self.mse(
                 torch.flatten(
-                    input=object_not_exists * predictions[..., score_idx_start + (5 * i):score_idx_end + (5 * i)],
+                    input=object_not_exists * predictions[..., 5 * i:1 + (5 * i)],
                     start_dim=1
                 ),
                 torch.flatten(
-                    input=object_not_exists * target[..., score_idx_start:score_idx_end],
+                    input=object_not_exists * target[..., 0:1],
                     start_dim=1
                 )
             )
 
-        # CLASS LOSS
-        class_loss = self.mse(
-            torch.flatten(object_exists * predictions[..., :self.num_classes], end_dim=-2, ),
-            torch.flatten(object_exists * target[..., :self.num_classes], end_dim=-2, ),
-        )
-
         loss = (
                 self.lambda_coord * box_loss
-                + object_loss
+                + self.lambda_obj * object_loss
                 + self.lambda_noobj * no_object_loss
-                + class_loss
         )
 
-        return loss, box_loss, object_loss, no_object_loss, class_loss
+        return loss, box_loss, object_loss, no_object_loss
 
     def _collect_predictions(self, predictions: Tensor, best_box: Tensor, start_idx: int, end_idx: int) -> Tensor:
         collection = torch.zeros_like(predictions[..., start_idx:end_idx])

src/trainer.py
@@ -5,6 +5,7 @@ from typing import Dict, Any, Optional, Tuple
 import torch
 from torch import Tensor
 from torch.nn import Module
+from torch.nn.utils import clip_grad_norm_
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -35,18 +36,18 @@ class Trainer:
         self.device = device
 
         learning_rate = config["learning_rate"]
-        self.num_classes = config["classes"]
         self.iou_threshold = config["iou_threshold"]
         self.conf_threshold = config["conf_threshold"]
         self.epochs = config["epochs"]
         self.eval_interval = config["eval_interval"]
         self.checkpoint_interval = config["checkpoint_interval"]
+        self.max_grad_norm = config["max_grad_norm"]
 
         self.model = Yolo(config).to(device)
         self.optimizer = Adam(self.model.parameters(), lr=learning_rate)
         self.criterion = YoloLoss(config)
 
-        self.save_dir = save_dir
+        self.save_dir = save_dir / self.logger.tensorboard_dir.name
         makedirs(self.save_dir, exist_ok=True)
 
         if checkpoint_path is not None:
@@ -56,7 +57,7 @@ class Trainer:
         self.logger.info(f"Number of trainable parameters: {count_parameters(self.model)}")
 
         self.train_dl, self.val_dl, self.test_dl = dataloaders
-        self.best_score_metric = "mAP"
+        self.best_score_metric = "F1"
         self.best_score = 0
 
         self.detector = Detector(config, self.model)
@@ -78,6 +79,7 @@ class Trainer:
         for batch in tqdm(self.train_dl):
             loss, _ = self._forward(batch)
             loss.backward()
+            clip_grad_norm_(self.model.parameters(), max_norm=self.max_grad_norm)
             self.optimizer.step()
 
         self.logger.log_epoch(mode="train", epoch=epoch)
@@ -108,7 +110,6 @@ class Trainer:
             predicted_bboxes = self.detector.cellboxes_to_boxes(output)
 
             batch_size = source.shape[0]
-
             for idx in range(batch_size):
                 nms_boxes = non_max_suppression(
                     predicted_bboxes[idx],
@@ -120,12 +121,12 @@ class Trainer:
                     all_predicted_boxes.append([entry_idx] + nms_box)
 
                 for box in target_bboxes[idx]:
-                    if box[1] > threshold:
+                    if box[0] > threshold:
                         all_target_boxes.append([entry_idx] + box)
 
                 entry_idx += 1
 
-        metrics = compute_metrics(all_predicted_boxes, all_target_boxes, iou_threshold, self.num_classes)
+        metrics = compute_metrics(all_predicted_boxes, all_target_boxes, iou_threshold)
         self.logger.log_epoch(mode="eval", epoch=epoch, metrics=metrics)
 
         if epoch is not None:

src/utils.py
@@ -68,8 +68,8 @@ def intersection_over_union(prediction: Tensor, target: Tensor) -> Tensor:
 
 
 def non_max_suppression(bboxes: List[List[float]], iou_threshold: float, threshold: float) -> List[List[float]]:
-    bboxes = [box for box in bboxes if box[1] > threshold]
-    bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
+    bboxes = [box for box in bboxes if box[0] > threshold]
+    bboxes = sorted(bboxes, key=lambda x: x[0], reverse=True)
     bboxes_after_nms = []
 
     while bboxes:
@@ -77,9 +77,9 @@ def non_max_suppression(bboxes: List[List[float]], iou_threshold: float, thresho
 
         bboxes = [
             box for box in bboxes
-            if box[0] != chosen_box[0] or intersection_over_union(
-                torch.tensor(chosen_box[2:]),
-                torch.tensor(box[2:]),
+            if intersection_over_union(
+                torch.tensor(chosen_box[1:]),
+                torch.tensor(box[1:]),
             ) < iou_threshold
         ]
 
@@ -92,98 +92,67 @@ def compute_metrics(
         pred_boxes: List,
         gt_boxes: List,
         iou_threshold: float,
-        num_classes: int,
 ) -> Dict[str, float]:
     epsilon = 1e-6
-    metrics_per_class = {}
 
-    for class_id in range(num_classes):
-        detections = [box for box in pred_boxes if box[1] == class_id]
-        ground_truths = [box for box in gt_boxes if box[1] == class_id]
+    detections = [box for box in pred_boxes]
+    ground_truths = [box for box in gt_boxes]
 
-        gt_box_per_entry_counter = dict(Counter([box[0] for box in ground_truths]))
+    gt_box_per_entry_counter = dict(Counter([box[0] for box in ground_truths]))
 
-        # Replace the count with a 0-vector
-        for key, value in gt_box_per_entry_counter.items():
-            gt_box_per_entry_counter[key] = torch.zeros(value)
+    # Replace the count with a 0-vector
+    for key, value in gt_box_per_entry_counter.items():
+        gt_box_per_entry_counter[key] = torch.zeros(value)
 
-        detections.sort(key=lambda x: x[2], reverse=True)
-        tp = torch.zeros((len(detections)))
-        fp = torch.zeros((len(detections)))
-        fn = torch.zeros((len(detections)))
-        total_true_bboxes = len(ground_truths)
+    detections.sort(key=lambda x: x[1], reverse=True)
+    tps = torch.zeros((len(detections)))
+    fps = torch.zeros((len(detections)))
+    total_true_bboxes = len(ground_truths)
 
-        # If none exists for this class then we can safely skip
-        if total_true_bboxes == 0:
-            continue
+    for detection_idx, detection in enumerate(detections):
+        entry_idx = detection[0]
+        entry_gt_boxes = [bbox for bbox in ground_truths if bbox[0] == entry_idx]
 
-        for detection_idx, detection in enumerate(detections):
-            entry_idx = detection[0]
-            entry_gt_boxes = [bbox for bbox in ground_truths if bbox[0] == entry_idx]
+        best_iou, best_gt_idx = 0, 0
 
-            best_iou, best_gt_idx = 0, 0
+        for gt_idx, gt in enumerate(entry_gt_boxes):
+            iou = intersection_over_union(
+                torch.tensor(detection[2:]),
+                torch.tensor(gt[2:]),
+            )
 
-            for gt_idx, gt in enumerate(entry_gt_boxes):
-                iou = intersection_over_union(
-                    torch.tensor(detection[3:]),
-                    torch.tensor(gt[3:]),
-                )
+            if iou > best_iou:
+                best_iou = iou
+                best_gt_idx = gt_idx
 
-                if iou > best_iou:
-                    best_iou = iou
-                    best_gt_idx = gt_idx
-
-            if best_iou > iou_threshold:
-                if gt_box_per_entry_counter[entry_idx][best_gt_idx] == 0:
-                    tp[detection_idx] = 1
-                    gt_box_per_entry_counter[entry_idx][best_gt_idx] = 1
-                else:
-                    fp[detection_idx] = 1
+        if best_iou > iou_threshold:
+            if gt_box_per_entry_counter[entry_idx][best_gt_idx] == 0:
+                tps[detection_idx] = 1
+                gt_box_per_entry_counter[entry_idx][best_gt_idx] = 1
             else:
-                fn[detection_idx] = 1
+                fps[detection_idx] = 1
+        else:
+            fps[detection_idx] = 1
 
-        tp_cum_sum = torch.cumsum(tp, dim=0) if tp.numel() != 0 else Tensor([0, ])
-        fp_cum_sum = torch.cumsum(fp, dim=0) if fp.numel() != 0 else Tensor([0, ])
-        fn_cum_sum = torch.cumsum(fn, dim=0) if fn.numel() != 0 else Tensor([0, ])
+    tp_cum_sum = torch.cumsum(tps, dim=0) if tps.numel() != 0 else Tensor([0, ])
+    fp_cum_sum = torch.cumsum(fps, dim=0) if fps.numel() != 0 else Tensor([0, ])
 
-        recalls = torch.divide(tp_cum_sum, (tp_cum_sum + fn_cum_sum + epsilon))
-        recalls = torch.cat((torch.tensor([0]), recalls))
+    recalls = torch.divide(tp_cum_sum, (total_true_bboxes + epsilon))
+    recalls = torch.cat((torch.tensor([0]), recalls))
 
-        precisions = torch.divide(tp_cum_sum, (tp_cum_sum + fp_cum_sum + epsilon))
-        precisions = torch.cat((torch.tensor([1]), precisions))
+    precisions = torch.divide(tp_cum_sum, (tp_cum_sum + fp_cum_sum + epsilon))
+    precisions = torch.cat((torch.tensor([1]), precisions))
 
-        ap = torch.trapz(precisions, recalls)
+    ap = torch.trapz(precisions, recalls)
+    f1 = 2 * (precisions * recalls) / (precisions + recalls + epsilon)
 
-        # Calculate F1 score
-        f1 = 2 * (precisions * recalls) / (precisions + recalls + epsilon)
-
-        # Store metrics for the class
-        metrics_per_class[class_id] = {
-            "AP": ap.item(),
-            "Precision": precisions[-1].item(),
-            "Recall": recalls[-1].item(),
-            "F1": f1[-1].item(),
-            "TP": tp_cum_sum[-1].item(),
-            "FP": fp_cum_sum[-1].item(),
-            "FN": fn_cum_sum[-1].item(),
-        }
-
-    aggregated_metrics = {
-        "mAP": _aggregate_mean(metrics_per_class, metric="AP"),
-        "Precision": _aggregate_mean(metrics_per_class, metric="Precision"),
-        "Recall": _aggregate_mean(metrics_per_class, metric="Recall"),
-        "F1": _aggregate_mean(metrics_per_class, metric="F1"),
-        "TP": _aggregate_sum(metrics_per_class, metric="TP"),
-        "FP": _aggregate_sum(metrics_per_class, metric="FP"),
-        "FN": _aggregate_sum(metrics_per_class, metric="FN"),
+    metrics = {
+        "AP": ap.item(),
+        "Precision": precisions[-1].item(),
+        "Recall": recalls[-1].item(),
+        "F1": f1[-1].item(),
+        "TP": tp_cum_sum[-1].item(),
+        "FP": fp_cum_sum[-1].item(),
     }
 
-    return aggregated_metrics
-
-
-def _aggregate_sum(metrics_per_class: Dict[int, Dict[str, Any]], metric: str) -> float:
-    return sum(metrics_per_class[class_id][metric] for class_id in metrics_per_class.keys())
-
-
-def _aggregate_mean(metrics_per_class: Dict[int, Dict[str, Any]], metric: str) -> float:
-    return _aggregate_sum(metrics_per_class, metric) / len(metrics_per_class.keys())
+    return metrics